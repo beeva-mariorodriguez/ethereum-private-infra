9c8070ca7ab7d5c6b267649945b4633233e147f2
beeva-mariorodriguez <mario.rodriguez@beeva.com>
2018-02-28 10:59:08 +0100

    geth-1.8.1

--- scripts/setup-vm.sh
+++ scripts/setup-vm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo apt install -y jq
-wget https://gethstore.blob.core.windows.net/builds/geth-alltools-linux-amd64-1.7.3-4bb3c89d.tar.gz
-tar xf geth-alltools-linux-amd64-1.7.3-4bb3c89d.tar.gz
-sudo cp geth-alltools-linux-amd64-1.7.3-4bb3c89d/* /usr/local/bin
+sudo apt-get install -y jq
+wget https://gethstore.blob.core.windows.net/builds/geth-alltools-linux-amd64-1.8.1-1e67410e.tar.gz
+tar xf geth-alltools-linux-amd64-1.8.1-1e67410e.tar.gz
+sudo cp geth-alltools-linux-amd64-1.8.1-1e67410e/* /usr/local/bin
 
