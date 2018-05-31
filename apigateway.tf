811c2496d2a6eee527504630a15a9c8818e91398
beeva-mariorodriguez <mario.rodriguez@beeva.com>
2018-05-31 15:15:39 +0200

    fix API gateway bugs

--- apigateway.tf
+++ apigateway.tf
@@ -7,14 +7,15 @@ resource "aws_api_gateway_rest_api" "ethereum" {
 resource "aws_api_gateway_resource" "enode" {
   rest_api_id = "${aws_api_gateway_rest_api.ethereum.id}"
   parent_id   = "${aws_api_gateway_rest_api.ethereum.root_resource_id}"
-  path_part   = "/enode"
+  path_part   = "enode"
 }
 
 resource "aws_api_gateway_method" "enode" {
-  rest_api_id   = "${aws_api_gateway_rest_api.ethereum.id}"
-  resource_id   = "${aws_api_gateway_resource.enode.id}"
-  http_method   = "ANY"
-  authorization = "NONE"
+  rest_api_id      = "${aws_api_gateway_rest_api.ethereum.id}"
+  resource_id      = "${aws_api_gateway_resource.enode.id}"
+  http_method      = "ANY"
+  authorization    = "NONE"
+  api_key_required = false
 }
 
 resource "aws_api_gateway_integration" "enode" {
@@ -24,35 +25,35 @@ resource "aws_api_gateway_integration" "enode" {
 
   integration_http_method = "ANY"
   type                    = "HTTP_PROXY"
-  uri                     = "${aws_instance.ethereum_proxy.public_ip}:8080"
+  uri                     = "http://${aws_instance.ethereum_proxy.public_ip}:8080"
 }
 
 # /json
 resource "aws_api_gateway_resource" "json" {
   rest_api_id = "${aws_api_gateway_rest_api.ethereum.id}"
   parent_id   = "${aws_api_gateway_rest_api.ethereum.root_resource_id}"
-  path_part   = "/json"
+  path_part   = "json"
 }
 
 resource "aws_api_gateway_method" "json" {
-  rest_api_id   = "${aws_api_gateway_rest_api.ethereum.id}"
-  resource_id   = "${aws_api_gateway_resource.json.id}"
-  http_method   = "ANY"
-  authorization = "NONE"
+  rest_api_id      = "${aws_api_gateway_rest_api.ethereum.id}"
+  resource_id      = "${aws_api_gateway_resource.json.id}"
+  http_method      = "ANY"
+  authorization    = "NONE"
+  api_key_required = false
 }
 
 resource "aws_api_gateway_integration" "json" {
   rest_api_id = "${aws_api_gateway_rest_api.ethereum.id}"
-  resource_id = "${aws_api_gateway_method.enode.resource_id}"
-  http_method = "${aws_api_gateway_method.enode.http_method}"
+  resource_id = "${aws_api_gateway_method.json.resource_id}"
+  http_method = "${aws_api_gateway_method.json.http_method}"
 
   integration_http_method = "ANY"
   type                    = "HTTP_PROXY"
-  uri                     = "${aws_instance.bastion.public_ip}:8080/InnovationDay.json"
+  uri                     = "http://${aws_instance.bastion.public_ip}:8080/InnovationDay.json"
 }
 
 # deployment
-
 resource "aws_api_gateway_deployment" "ethereum" {
   depends_on = [
     "aws_api_gateway_integration.enode",
